45ebf9944b889cde800b097fa2d760075c8a7737
Felipe Gaitan <dfelipegaitanh@gmail.com>
2024-11-28 22:45:31 -0500

    Add Last.fm user fetching route and update dashboard redirect
    Introduced a new route group under 'last-fm' with a route to fetch Last.fm user data using the 'GetUser' Livewire component. Updated the 'dashboard' route to redirect to the new Last.fm user fetching route, ensuring authorized and verified users are directed there upon accessing the dashboard.

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,24 @@
 <?php
 
+use App\Livewire\LastFm\GetUser;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('login');
 });
 
-Route::view('dashboard', 'dashboard')
+Route::prefix('last-fm')
+    ->name('last-fm.')
+    ->group(function () {
+
+        Route::get('/get-user', GetUser::class)
+            ->name('get-user');
+
+    });
+
+Route::get('dashboard', function () {
+    return redirect()->route('last-fm.get-user');
+})
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
