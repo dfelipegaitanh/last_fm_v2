3c7004c857069128aae8d7a80c8a1fa1ec09df88
Felipe Gaitan <dfelipegaitanh@gmail.com>
2024-12-18 00:08:36 -0500

    Update LastFM user info display logic and header
    Replaced null check with empty check for better reliability when handling user data. Updated header to include serialized JSON representation of the LastFM user for clarity. These changes enhance the robustness and readability of the information displayed.

--- resources/views/livewire/last-fm/get-user/user-info.blade.php
+++ resources/views/livewire/last-fm/get-user/user-info.blade.php
@@ -1,6 +1,6 @@
 <div class=" bg-white shadow-md rounded-lg overflow-hidden border border-gray-200 p-6">
-    <h2 class="text-xl font-semibold text-gray-800 mb-2">Información de LastFM</h2>
-    @if ($lastFmUser !== null)
+    <h2 class="text-xl font-semibold text-gray-800 mb-2">Información de LastFM {{ collect($lastFmUser)->toJson() }}</h2>
+    @if (!empty($lastFmUser))
         <p class="text-gray-600">Nombre del usuario: {{ $lastFmUser['name'] }}</p>
         <p class="text-gray-600">País: {{ $lastFmUser['country'] }}</p>
         <p class="text-gray-600">Fecha de registro: {{ $lastFmUser['registered'] }}</p>
