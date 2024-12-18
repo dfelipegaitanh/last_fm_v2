257955cabdc4be921e4b434a9beeed4b1aab3de2
Felipe Gaitan <dfelipegaitanh@gmail.com>
2024-12-18 00:08:25 -0500

    Add action to save global song statistics for users
    Introduced `saveGlobalSongsStatisticsAction` to handle saving global song statistics for LastFm users. This ensures that data such as playcount, artist count, track count, and album count are stored or updated correctly in the database. The implementation uses `firstOrCreate` for efficient upserts.

---
+++ app/Actions/LastFmUser/saveGlobalSongsStatisticsAction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Actions\LastFmUser;
+
+use App\Models\LastFmGlobalSongsStatistics;
+use App\Models\LastFmUser;
+
+class saveGlobalSongsStatisticsAction
+{
+
+    public function execute(LastFmUser $lastFmUser, array $userInfo): void
+    {
+        $userInfo['last_fm_user_id'] = $lastFmUser->id;
+        LastFmGlobalSongsStatistics::firstOrCreate(
+            [
+                'last_fm_user_id' => $userInfo['last_fm_user_id'],
+                'playcount'       => $userInfo['playcount'],
+                'artist_count'    => $userInfo['artist_count'],
+                'track_count'     => $userInfo['track_count'],
+                'album_count'     => $userInfo['album_count'],
+            ], $userInfo,
+        );
+    }
+
+}
