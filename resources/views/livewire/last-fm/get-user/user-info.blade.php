96f9697d29232cf576db5ff76aa76e061eb0ad20
Felipe Gaitan <dfelipegaitanh@gmail.com>
2024-12-18 22:53:16 -0500

    Add loading states to LastFM user info component
    Introduced `wire:loading` and `wire:loading.remove` directives for better user experience during data loading. Added a spinner placeholder to visually indicate loading progress. Updated HTML structure for improved readability and maintainability.

--- resources/views/livewire/last-fm/get-user/user-info.blade.php
+++ resources/views/livewire/last-fm/get-user/user-info.blade.php
@@ -1,5 +1,9 @@
-<div class=" bg-white shadow-md rounded-lg overflow-hidden border border-gray-200 p-6 {{ empty($lastFmUser) ? 'disabled-div' : '' }}">
+<div
+        wire:loading.class="disabled-div"
+        class=" bg-white shadow-md rounded-lg overflow-hidden border border-gray-200
+    p-6 {{ empty($lastFmUser) ? 'disabled-div' : '' }}">
     <h2 class="text-xl font-semibold text-gray-800 mb-2">Información de LastFM</h2>
+    <div wire:loading.remove>
     @if (!empty($lastFmUser))
         <p class="text-gray-600">Nombre del usuario: {{ $lastFmUser['name'] }}</p>
         <p class="text-gray-600">País: {{ $lastFmUser['country'] }}</p>
@@ -7,4 +11,10 @@
     @else
         <p class="text-gray-500">La información del usuario de LastFM aún no está disponible.</p>
     @endif
+    </div>
+    <div class="flex justify-center items-center">
+        <div wire:loading>
+            <livewire:placeholder.spinner-body/>
+        </div>
+    </div>
 </div>
