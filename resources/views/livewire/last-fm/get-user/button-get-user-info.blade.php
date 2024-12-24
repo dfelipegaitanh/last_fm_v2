d49c1cad84fef4ecb7437a0bc412a8590f1e5d9f
Felipe.Gaitan <felipe.gaitan@quoruminfotech.com>
2024-12-24 10:36:48 -0500

    Update button layouts and add 'fetch songs' functionality
    Enhanced button styling for consistent width and spacing, improving layout structure. Introduced a new button to fetch songs with appropriate UI consistency and behavior.

--- resources/views/livewire/last-fm/get-user/button-get-user-info.blade.php
+++ resources/views/livewire/last-fm/get-user/button-get-user-info.blade.php
@@ -1,14 +1,19 @@
-<div class="overflow-hidden">
+<div class="overflow-hidden flex flex-col gap-4 justify-start mb-4 w-full max-w-md">
     <button wire:click.debounce="$dispatch('userInfo:updateLastFmUser')"
             type="button"
-            class="px-5 py-3 bg-indigo-500 text-white font-medium rounded-md shadow-md hover:bg-indigo-400 hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-purple-300 focus:ring-opacity-50 active:bg-indigo-600 active:ring-purple-400 transition duration-300 transform active:scale-95">
+            class="w-full px-5 py-3 bg-indigo-500 text-white font-medium rounded-md shadow-md hover:bg-indigo-400 hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-purple-300 focus:ring-opacity-50 active:bg-indigo-600 active:ring-purple-400 transition duration-300 transform active:scale-95">
         Get User Info
     </button>
+
+    <button wire:click.debounce="$dispatch('userInfo:fetchSongs')"
+            type="button"
+            class="w-full px-5 py-3 bg-indigo-500 text-white font-medium rounded-md shadow-md hover:bg-indigo-400 hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-purple-300 focus:ring-opacity-50 active:bg-indigo-600 active:ring-purple-400 transition duration-300 transform active:scale-95">
+        Traer canciones
+    </button>
+
     <button wire:click.debounce="$dispatch('userInfo:clearLastFmUser')"
             type="button"
-            class="px-5 py-3 bg-red-500 text-white font-medium rounded-md shadow-md hover:bg-red-400 hover:shadow-lg
-    focus:outline-none focus:ring-4 focus:ring-pink-300 focus:ring-opacity-50 active:bg-red-600 active:ring-pink-400
-    transition duration-300 transform active:scale-95">
+            class="w-full px-5 py-3 bg-red-500 text-white font-medium rounded-md shadow-md hover:bg-red-400 hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-pink-300 focus:ring-opacity-50 active:bg-red-600 active:ring-pink-400 transition duration-300 transform active:scale-95">
         Clear
     </button>
 </div>
