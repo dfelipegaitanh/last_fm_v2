b7fcc6342edfd15f5fc60823ef473ebce1ebad6d
Felipe.Gaitan <felipe.gaitan@quoruminfotech.com>
2024-12-24 11:53:26 -0500

    Remove unused ButtonGetUserInfo component and update button logic
    Deleted the obsolete `ButtonGetUserInfo` Livewire component to clean up unused code. Updated button click logic to use a more specific event name and parameters for clarity. Additionally, fixed a typo in the spinner view filename for consistency.

--- resources/views/livewire/index/buttons.blade.php
+++ resources/views/livewire/index/buttons.blade.php
@@ -5,7 +5,7 @@
         Get User Info
     </button>
 
-    <button wire:click.debounce="$dispatch('userInfo:fetchSongs')"
+    <button wire:click.debounce="$dispatch('getSongs:fetchSongs', { type : 'daily' })"
             type="button"
             class="w-full px-5 py-3 bg-indigo-500 text-white font-medium rounded-md shadow-md hover:bg-indigo-400 hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-purple-300 focus:ring-opacity-50 active:bg-indigo-600 active:ring-purple-400 transition duration-300 transform active:scale-95">
         Traer canciones
