5d781a64952663d1cb2fb242c4cdf13978eda9f5
Felipe Gaitan <dfelipegaitanh@gmail.com>
2024-12-31 15:44:31 -0500

    Refactor layout for main page content structure.
    Moved layout structure from Livewire component to the main Blade layout file for better reusability and consistency. Simplified the Livewire view by removing redundant container and layout code.

--- resources/views/livewire/last-fm/get-user/index.blade.php
+++ resources/views/livewire/last-fm/get-user/index.blade.php
@@ -1,14 +1,3 @@
-<div class="container mx-auto p-10">
-    <div class="flex flex-row space-x-4">
-
-        <div class="w-1/4 flex justify-center">
-            <livewire:index.buttons/>
-        </div>
-
-        <div class="w-3/4">
-            <livewire:last-fm.get-user.user-info/>
-        </div>
-
-    </div>
-
+<div>
+    <livewire:last-fm.get-user.user-info/>
 </div>
