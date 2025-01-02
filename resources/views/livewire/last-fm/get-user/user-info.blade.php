368514866ad33c68d6db582a37cde166816145be
Felipe Gaitan <dfelipegaitanh@gmail.com>
2025-01-02 16:37:23 -0500

    Refactor LastFM user info and statistics UI components
    Improve the structure and design of the user info placeholder and statistics table. Added enhanced styling, optimized conditional rendering, and updated table rows for better readability and user experience.

--- resources/views/livewire/last-fm/get-user/user-info.blade.php
+++ resources/views/livewire/last-fm/get-user/user-info.blade.php
@@ -1,25 +1,32 @@
-<div
-    wire:loading.class="disabled-div"
-    class=" bg-white shadow-md rounded-lg overflow-hidden border border-gray-200
-    p-6 {{ empty($lastFmUser) ? 'disabled-div' : '' }}">
-    <h2 class="text-xl font-semibold text-gray-800 mb-2">Información de LastFM</h2>
-    <div wire:loading.remove>
-        <div class="mb-4">
-            @if (!empty($lastFmUser))
-                <p class="text-gray-600">Nombre del usuario: {{ $lastFmUser->name }}</p>
-                <p class="text-gray-600">País: {{ $lastFmUser->country }}</p>
-                <p class="text-gray-600">Fecha de registro: {{ $lastFmUser->registered }}</p>
-                <livewire:last-fm.statistics.global-statistics :lastFmUser="$lastFmUser"/>
-            @else
+<div wire:loading.class="disabled-div"
+     class=" bg-white shadow-md rounded-lg overflow-hidden border border-gray-200 p-6">
+    <div class="{{ empty($lastFmUser) ? 'disabled-div' : '' }}">
+        <div class="flex flex-col items-center justify-center text-center py-8 {{ !empty($lastFmUser) ? 'hidden' : '' }} ">
+            <svg class="w-16 h-16 text-gray-400 mb-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                 stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M9 17v-4H5v4m14 0v-4h-4v4m4 0h-4m4 0a2 2 0 100-4 2 2 0 100 4zM5 17a2 2 0 100-4 2 2 0 100 4zM5 13v-2a7 7 0 0114 0v2"></path>
+            </svg>
+            <div class="border-2 border-dashed border-gray-300 rounded-lg p-6 text-center">
                 <p class="text-gray-500">La información del usuario de LastFM aún no está disponible.</p>
-            @endif
+            </div>
         </div>
-    </div>
+        <div wire:loading.remove>
+            <div class="mb-4">
+                @if (!empty($lastFmUser))
+                    <p class="text-gray-600">Nombre del usuario: {{ $lastFmUser->name }}</p>
+                    <p class="text-gray-600">País: {{ $lastFmUser->country }}</p>
+                    <p class="text-gray-600">Fecha de registro: {{ $lastFmUser->registered }}</p>
+                    <livewire:last-fm.statistics.global-statistics :lastFmUser="$lastFmUser"/>
+                @endif
+            </div>
+        </div>
 
 
-    <div class="flex justify-center items-center">
-        <div wire:loading>
-            <livewire:placeholder.spinner-body/>
+        <div class="flex justify-center items-center">
+            <div wire:loading>
+                <livewire:placeholder.spinner-body/>
+            </div>
         </div>
     </div>
 
--- resources/views/livewire/last-fm/statistics/global-statistics.blade.php
+++ resources/views/livewire/last-fm/statistics/global-statistics.blade.php
@@ -2,25 +2,22 @@
 
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-300">
             <tr>
-                <th scope="col" class="px-6 py-3">
-                    Play Count
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Play Count
                 </th>
-                <th scope="col" class="px-6 py-3">
-                    Album Count
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Album Count
                 </th>
-                <th scope="col" class="px-6 py-3">
-                    Artist Count
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Artist
+                    Count
                 </th>
-                <th scope="col" class="px-6 py-3">
-                    Created At
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Created At
                 </th>
             </tr>
             </thead>
             <tbody>
             @foreach($this->statistics as $statistic)
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <tr class="{{ $loop->even ? 'bg-gray-50 dark:bg-gray-800' : 'bg-white dark:bg-gray-900' }} hover:bg-gray-100 dark:hover:bg-gray-700">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         {{ $statistic->playcount }}
                     </th>
@@ -35,11 +32,8 @@
                     </td>
                 </tr>
             @endforeach
-
-
             </tbody>
         </table>
     </div>
-
 
 </div>
