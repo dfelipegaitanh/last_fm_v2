c112f59af00e04676d0dacce629c214a0cf2dd34
Felipe Gaitan <dfelipegaitanh@gmail.com>
2025-01-03 17:10:40 -0500

    Add fetch data button with loading state to chart component
    Introduced a button to allow fetching data in the chart component, with a loading spinner for better UX. This update also includes handling of button visibility and lazy loading of the component.

--- app/Livewire/LastFm/GetSongs/Reports/Chart.php
+++ app/Livewire/LastFm/GetSongs/Reports/Chart.php
@@ -2,10 +2,24 @@
 
 namespace App\Livewire\LastFm\GetSongs\Reports;
 
+use Livewire\Attributes\Lazy;
 use Livewire\Component;
 
+#[Lazy]
 class Chart extends Component
 {
+
+    public bool $showFetchDataButton;
+
+    public function mount()
+    {
+        $this->showFetchDataButton = true;
+    }
+
+    public function fetchData()
+    {
+        $this->showFetchDataButton = false;
+    }
     public function render()
     {
         return view('livewire.last-fm.get-songs.reports.chart');
