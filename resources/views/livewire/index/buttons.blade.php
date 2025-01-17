7c0c704c9a0398c5c0691a2b8b9b6f53ef98ecbd
Felipe.Gaitan <felipe.gaitan@quoruminfotech.com>
2025-01-17 10:23:56 -0500

    Remove SidebarButton component and replace with SidebarSvgButton
    The `SidebarButton` component was removed in favor of a more suitable `SidebarSvgButton` component. All references were updated accordingly, and the blade file was renamed to reflect this change. This improves component clarity and ensures consistency in naming conventions.

--- resources/views/livewire/index/buttons.blade.php
+++ resources/views/livewire/index/buttons.blade.php
@@ -1,22 +1,22 @@
 <div class="overflow-hidden flex flex-col gap-4 justify-start mb-4 w-full max-w-md">
 
-    <x-sidebar-button
+    <x-lastfm.sidebar-svg-button
             text="Get User Info"
             route="last-fm.get-user"
             color="indigo">
         M5.121 13.934A1.5 1.5 0 016 13h12a1.5 1.5 0 011.5 1.5V17a1.5 1.5 0 01-1.5 1.5H6a1.5 1.5 0 01-1.5-1.5v-3.066zM12
         9a4 4 0 100-8 4 4 0 000 8z
-    </x-sidebar-button>
+    </x-lastfm.sidebar-svg-button>
 
     <div class="w-full border-t border-gray-300"></div>
 
-    <x-sidebar-button
+    <x-lastfm.sidebar-svg-button
             text="Fetch Songs"
             route="last-fm.get-songs"
             color="blue">
         m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0
         0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 0
         1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z
-    </x-sidebar-button>
+    </x-lastfm.sidebar-svg-button>
 
 </div>
