dbda30e35785ce33473bcc9a2839874a6d7d688b
Felipe Gaitan <dfelipegaitanh@gmail.com>
2025-01-19 17:38:38 -0500

    Actualiza clases CSS y estructura de colores en Tailwind
    Se renombraron varias clases de Tailwind para una nomenclatura más consistente y descriptiva en estilos claros y oscuros. También se reorganizó la configuración de colores en `tailwind.config.js` para simplificar la estructura y mejorar la claridad del código. Esto facilita el mantenimiento y mejora la legibilidad general del proyecto.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,121 +10,71 @@ export default {
         './resources/views/**/*.blade.php',
     ],
 
-    // darkMode: 'selector',
     darkMode: 'class',
 
     theme: {
         extend: {
             colors: {
+                // Colores para el modo claro
+                'light': {
+                    // Fondo y texto
+                    'background': '#f3f4f6', // gray-100
+                    'text': '#1f2937', // gray-800
 
-                // $blue-50: #eff6ff;
-                // $blue-100: #dbeafe;
-                // $blue-200: #bfdbfe;
-                // $blue-300: #93c5fd;
-                // $blue-400: #60a5fa;
-                // $blue-500: #3b82f6;
-                // $blue-600: #2563eb;
-                // $blue-700: #1d4ed8;
-                // $blue-800: #1e40af;
-                // $blue-900: #1e3a8a;
+                    // Header
+                    'header-bg': '#ffffff', // white
 
-                // $gray-50: #f9fafb;
-                // $gray-100: #f3f4f6;
-                // $gray-200: #e5e7eb;
-                // $gray-300: #d1d5db;
-                // $gray-400: #9ca3af;
-                // $gray-500: #6b7280;
-                // $gray-600: #4b5563;
-                // $gray-700: #374151;
-                // $gray-800: #1f2937;
-                // $gray-900: #111827;
+                    // Navegación
+                    'nav-bg': '#ffffff', // white
+                    'nav-border': '#f3f4f6', // gray-100
 
-                // $indigo-50: #eef2ff;
-                // $indigo-100: #e0e7ff;
-                // $indigo-200: #c7d2fe;
-                // $indigo-300: #a5b4fc;
-                // $indigo-400: #818cf8;
-                // $indigo-500: #6366f1;
-                // $indigo-600: #4f46e5;
-                // $indigo-700: #4338ca;
-                // $indigo-800: #3730a3;
-                // $indigo-900: #312e81;
+                    // Navegación - Active
+                    'nav-link-active-border': '#6366f1', // indigo-500
+                    'nav-link-active-hover': '#6366f1', // indigo-500
+                    'nav-link-active-text': '#111827', // gray-900
+                    // Navegación - Inactive
+                    'nav-link-inactive-focus': '#374151', // gray-700
+                    'nav-link-inactive-hover': '#4b5563', // gray-600
+                    'nav-link-inactive-text': '#6b7280', // gray-500
 
+                    // Contenedor reutilizable
+                    'reusable-bg': '#ffffff', // white
 
-                'light-': {
-                    'bg': '#f3f4f6',
-                    'text': '#1f2937',
-
-                    'header': {
-                        'bg': '#ffffff',
-                    },
-                    'nav': {
-                        'bg': '#ffffff',
-                        'border': '#f3f4f6',
-
-                        'link': {
-                            'active-': {
-                                'border': '#6366f1',
-                                'hover': '#6366f1',
-                                'text': '#111827',
-                            },
-                            'inactive-': {
-                                'focus': '#374151',
-                                'hover': '#4b5563',
-                                'text': '#6b7280',
-                            }
-                        },
-
-                    },
-                    'reusable': {
-                        'bg': '#ffffff',
-                    },
-                    'svg': {
-                        'icon': {
-                            'primary': '#3b82f6',
-                            'secondary': '#3b82f6',
-                            'danger': '#ef4444',
-                        }
-                    },
-
+                    // Iconos SVG
+                    'svg-icon-primary': '#3b82f6', // blue-500
+                    'svg-icon-secondary': '#3b82f6', // blue-500
+                    'svg-icon-danger': '#ef4444', // red-500
                 },
+                // Colores para el modo oscuro
+                'dark': {
+                    // Fondo y texto
+                    'background': '#111827', // gray-900
+                    'text': '#e5e7eb', // gray-200
 
-                'dark-': {
-                    'bg': '#111827',
-                    'text': '#e5e7eb',
+                    // Header
+                    'header-bg': '#1f2937', // gray-800
 
-                    'header': {
-                        'bg': '#1f2937',
-                    },
-                    'nav': {
-                        'bg': '#1f2937',
-                        'border': '#374151',
-                        'link': {
-                            'active-': {
-                                'border': '#4338ca',
-                                'hover': '#818cf8',
-                                'text': '#e5e7eb',
-                            },
-                            'inactive-': {
-                                'focus': '#d1d5db',
-                                'text': '#9ca3af',
-                                'hover': '#6b7280',
-                            }
-                        },
-                    },
-                    'reusable': {
-                        'bg': '#1f2937',
-                    },
-                    'svg': {
-                        'icon': {
-                            'primary': '#93c5fd',
-                            'secondary': '#60a5fa',
-                            'danger': '#f87171',
-                        }
-                    },
+                    // Navegación
+                    'nav-bg': '#1f2937', // gray-800
+                    'nav-border': '#374151', // gray-700
+
+                    // Navegación - Active
+                    'nav-link-active-border': '#4338ca', // indigo-700
+                    'nav-link-active-hover': '#818cf8', // indigo-300
+                    'nav-link-active-text': '#e5e7eb', // gray-200
+                    // Navegación - Inactive
+                    'nav-link-inactive-focus': '#d1d5db', // gray-300
+                    'nav-link-inactive-hover': '#6b7280', // gray-500
+                    'nav-link-inactive-text': '#9ca3af', // gray-400
+
+                    // Contenedor reutilizable
+                    'reusable-bg': '#1f2937', // gray-800
+
+                    // Iconos SVG
+                    'svg-icon-primary': '#93c5fd', // blue-300
+                    'svg-icon-secondary': '#60a5fa', // blue-400
+                    'svg-icon-danger': '#f87171', // red-300
                 }
-
-
             },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
