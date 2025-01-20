df4433e453b1223938e21126360af8c6b15ae438
Felipe Gaitan <dfelipegaitanh@gmail.com>
2025-01-19 20:35:04 -0500

    "Añade estilos personalizados reutilizables para botones
    Se crea la clase '.buttons' en CSS para definir estilos base unificados y reutilizables para botones, incluyendo soporte para temas claros y oscuros. Además, se actualiza 'sidebar-svg-button.blade.php' para utilizar esta nueva clase, simplificando el código y asegurando consistencia visual."

--- resources/views/components/lastfm/sidebar-svg-button.blade.php
+++ resources/views/components/lastfm/sidebar-svg-button.blade.php
@@ -1,11 +1,10 @@
 <a wire:navigate
    href="{{ route($route) }}"
    wire:loading.attr="disabled"
-   class="px-5 py-3 bg-indigo-500 text-white font-medium rounded-md shadow-md
-           hover:bg-indigo-400 hover:shadow-lg focus:outline-none focus:ring-4
-           focus:ring-indigo-300 focus:ring-opacity-50 active:bg-indigo-600
-           active:ring-indigo-400 text-center transition duration-300 transform active:scale-90
-           justify-center gap-2 inline-flex items-center">
+   class="buttons"
+
+
+>
 
     <x-icon class="w-5 h-5"
             viewBox="0 0 24 24"
