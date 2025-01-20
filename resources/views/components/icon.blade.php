9a27592a2088fc4aa3bf91377984e71774e740f5
Felipe Gaitan <dfelipegaitanh@gmail.com>
2025-01-19 19:21:51 -0500

    "Refactoriza iconos SVG con el componente <x-icon> reutilizable
    Se ha creado un nuevo componente <x-icon> para manejar iconos SVG de forma más consistente y reutilizable. Se reemplazaron implementaciones directas de SVG en varias vistas por el nuevo componente, lo que mejora la legibilidad y facilita futuras modificaciones."

---
+++ resources/views/components/icon.blade.php
@@ -0,0 +1,26 @@
+@props([
+    'class' => '',
+    'viewBox' => '0 0 20 20',
+    'fill' => '',
+    'stroke' => 'currentColor',
+    'fill_rule' => '',
+    'clip_rule' => '',
+    'stroke_linecap' => '',
+    'stroke_linejoin' => '',
+    'stroke_width' => '',
+])
+
+<svg class="{{ $class }}"
+     xmlns="http://www.w3.org/2000/svg"
+     viewBox="{{ $viewBox }}"
+     stroke="{{ $stroke }}"
+     fill="{{ $fill }}"
+>
+    <path fill-rule="{{ $fill_rule }}"
+          clip-rule="{{ $clip_rule }}"
+          stroke-linecap="{{ $stroke_linecap }}"
+          stroke-linejoin="{{ $stroke_linejoin }}"
+          stroke-width="{{ $stroke_width }}"
+          d="{{ $slot }}"
+    />
+</svg>
--- resources/views/components/lastfm/sidebar-svg-button.blade.php
+++ resources/views/components/lastfm/sidebar-svg-button.blade.php
@@ -6,10 +6,15 @@
            focus:ring-indigo-300 focus:ring-opacity-50 active:bg-indigo-600
            active:ring-indigo-400 text-center transition duration-300 transform active:scale-90
            justify-center gap-2 inline-flex items-center">
-    <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="{{ $slot }}">
-        </path>
-    </svg>
+
+    <x-icon class="w-5 h-5"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke_linecap="round"
+            stroke_linejoin="round"
+            stroke_width="2"
+    >
+        {{ $slot }}
+    </x-icon>
     {{ $text }}
 </a>
