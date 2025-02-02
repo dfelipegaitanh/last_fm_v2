b3e7329cea384a321b113640d4bc39b967e26b2c
Felipe Gaitan <dfelipegaitanh@gmail.com>
2025-02-02 16:24:37 -0500

    "Agregadas migraciones para tablas 'last_fm_users', estadísticas globales y soporte de Telescope, además de ajuste en nombres de columnas."

---
+++ database/migrations/2024_12_17_214055_create_last_fm_global_songs_stadistics_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\LastFmUser;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('last_fm_global_songs_statistics', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(LastFmUser::class)->constrained('last_fm_users');
+            $table->string('playcount');
+            $table->string('artist_count');
+            $table->string('track_count');
+            $table->string('album_count');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('last_fm_global_songs_statistics');
+    }
+};
