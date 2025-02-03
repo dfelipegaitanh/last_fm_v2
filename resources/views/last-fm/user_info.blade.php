2c80503fec9ad7b74e82fb2e7ce8c896b8244118
Felipe Gaitan <dfelipegaitanh@gmail.com>
2025-02-03 17:38:37 -0500

    "Refactor: Crear componente Blade `<x-icon>` y reemplazar SVG inline en `user_info.blade.php` para mejorar reutilización y mantenibilidad."

--- resources/views/last-fm/user_info.blade.php
+++ resources/views/last-fm/user_info.blade.php
@@ -11,21 +11,7 @@
             <span x-text="showTable ? 'Cerrar Tabla' : 'Mostrar Tabla'"></span>
             <!-- Ícono SVG que solo aparece cuando el texto es "Cerrar Tabla" -->
             <template x-if="showTable">
-                <svg
-                    class="h-6 w-6 text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                >
-                    <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="m15 9-6 6m0-6 6 6m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                    />
-                </svg>
+                <x-icon d="m15 9-6 6m0-6 6 6m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </template>
         </button>
 
