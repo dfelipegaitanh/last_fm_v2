ba07d17db105c35cde42a6da7bec072bbdc2f933
Felipe Gaitan <dfelipegaitanh@gmail.com>
2025-02-18 16:49:34 -0500

    "Agregada la configuración de rutas para LastFm en el RouteServiceProvider y simplificada su definición en el archivo de rutas."

--- routes/last_fm.php
+++ routes/last_fm.php
@@ -4,18 +4,11 @@ use App\Http\Controllers\LastFm\UserGeInfoController;
 use App\Http\Controllers\LastFm\UserGetStatisticsController;
 use App\Http\Controllers\LastFm\UserInfoController;
 
-Route::middleware('auth:sanctum')
-    ->prefix('last-fm')
-    ->name('last-fm.')
-    ->group(function () {
+Route::get('user-info', UserInfoController::class)
+    ->name('user_info');
 
-        Route::get('user-info', UserInfoController::class)
-            ->name('user_info');
+Route::get('user-get-statistics', UserGetStatisticsController::class)
+    ->name('user_get_statistics');
 
-        Route::get('user-get-statistics', UserGetStatisticsController::class)
-            ->name('user_get_statistics');
-
-        Route::get('user-get-info', UserGeInfoController::class)
-            ->name('user_get_info');
-
-    });
+Route::get('user-get-info', UserGeInfoController::class)
+    ->name('user_get_info');
