967ce7deaf2b7db86c307eee49310a0479e0e356
Felipe.Gaitan <felipe.gaitan@quoruminfotech.com>
2025-02-18 14:58:15 -0500

    Add LastFmUserFactory for generating LastFmUser test data
    This factory enables the creation of LastFmUser models with predefined attributes for testing purposes. It includes faker-generated data like name, country, and user relationships, which enhances testing flexibility and efficiency.

---
+++ database/factories/LastFmUserFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\LastFmUser;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class LastFmUserFactory extends Factory
+{
+    protected $model = LastFmUser::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'subscriber' => $this->faker->boolean(),
+            'country' => $this->faker->country(),
+            'url' => $this->faker->url(),
+            'registered' => $this->faker->word(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'uuid' => $this->faker->uuid(),
+            'user_id' => User::factory(),
+        ];
+    }
+}
