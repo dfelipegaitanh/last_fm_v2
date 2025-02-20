05e3b63353a0c3f849034b5ed160dec82e1c7862
Felipe Gaitan <dfelipegaitanh@gmail.com>
2025-02-19 19:52:50 -0500

    "Corrige mayúsculas en el nombre de clase AuthenticatedUserDto y renombra parámetro para mayor claridad en el método fromModel."

--- app/DTO/LastFm/AuthenticatedUserDto.php
+++ app/DTO/LastFm/AuthenticatedUserDto.php
@@ -5,7 +5,7 @@ namespace App\DTO\LastFm;
 use App\Models\LastFmUser;
 use Spatie\LaravelData\Data;
 
-class AuthenticatedUserDTO extends Data
+class AuthenticatedUserDto extends Data
 {
     public function __construct(
         public ?string $name,
@@ -13,8 +13,12 @@ class AuthenticatedUserDTO extends Data
         public ?string $total_scrobbles,
     ) {}
 
-    public static function fromModel(LastFmUser $user): self
+    public static function fromModel(LastFmUser $lastFmUser): self
     {
+
+        $user = $lastFmUser->with('latestStatistic')
+            ->firstOrFail();
+
         return new self(
             name: $user->name,
             join_date: $user->registered,
