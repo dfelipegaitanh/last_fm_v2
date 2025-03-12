3333d79032ca39d40fd474246056a1c2d7db5dfc
Felipe Gaitan <dfelipegaitanh@gmail.com>
2025-03-12 16:41:39 -0400

    "Configura pint.json con reglas Laravel y ajustes adicionales a rector.php para mejorar tipado estricto, privatización y calidad del código."

--- rector.php
+++ rector.php
@@ -1,15 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 use Rector\Config\RectorConfig;
-use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 
 return RectorConfig::configure()
     // register single rule
-    ->withRules([
-        TypedPropertyFromStrictConstructorRector::class,
+    ->withSkip([
+        AddOverrideAttributeToOverriddenMethodsRector::class,
     ])
     // here we can define, what prepared sets of rules will be applied
     ->withPreparedSets(
         deadCode: true,
-        codeQuality: true
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true,
     );
