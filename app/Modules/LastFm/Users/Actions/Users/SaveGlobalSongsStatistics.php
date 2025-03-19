76e542b867e71c1560d77a8f0e20f1b8de70e4be
Felipe Gaitan <dfelipegaitanh@gmail.com>
2025-03-18 21:43:59 -0400

    "Refactor: Reemplazo LastFmService por LastFmApi y mejora en el manejo de estadísticas globales de canciones."

--- app/Modules/LastFm/Users/Actions/Users/SaveGlobalSongsStatistics.php
+++ app/Modules/LastFm/Users/Actions/Users/SaveGlobalSongsStatistics.php
@@ -7,34 +7,29 @@ namespace App\Modules\LastFm\Users\Actions\Users;
 use App\Models\User;
 use App\Modules\LastFm\Users\DTO\UserInfoDTO;
 use App\Modules\LastFm\Users\Models\GlobalSongsStatistics;
-use App\Services\LastFmService;
+use App\Services\Api\LastFm\LastFmApi;
 use Illuminate\Support\Arr;
 
 readonly class SaveGlobalSongsStatistics
 {
-    public function __construct(
-        private LastFmService $lastFmService)
-    {
-        $this->lastFmService
-            ->userRecentTrack();
-    }
+    //    public function __construct(
+    //        private LastFmApi $lastFmApi
+    //    ) {}
 
-    public function handle(User $user, UserInfoDTO $dto): void
+    public function handle(User $user, UserInfoDTO $userInfoDTO): GlobalSongsStatistics
     {
-
-        GlobalSongsStatistics::firstOrCreate(
-            $this->buildSearchAttributes($user, $dto),
-            $dto->toArray()
+        return GlobalSongsStatistics::firstOrCreate(
+            $this->buildSearchAttributes($user, $userInfoDTO),
+            $userInfoDTO->toArray()
         );
     }
 
-    private function buildSearchAttributes(User $user, UserInfoDTO $dto): array
+    private function buildSearchAttributes(User $user, UserInfoDTO $userInfoDTO): array
     {
-
         return Arr::add(
-            $dto->toStatisticsArray(),
+            $userInfoDTO->toStatisticsArray(),
             'last_fm_user_id',
-            $user->lastFmUser->id
+            $user->lastFmUser?->id ?? 0
         );
     }
 }
