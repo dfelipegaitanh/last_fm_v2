47ba817fe67da8afe24646164b844fe1d67f304e
Felipe Gaitan <dfelipegaitanh@gmail.com>
2025-03-21 14:22:46 -0400

    "Desactivar temporalmente LastFmRateLimitMiddleware en bootstrap/app.php para ajustar procesamiento de solicitudes."

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(LastFmRateLimitMiddleware::class);
+//        $middleware->append(LastFmRateLimitMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
