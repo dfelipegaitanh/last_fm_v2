ff1e945c7d1a3def2aac3206c015fc256a17b92c
Felipe Gaitan <dfelipegaitanh@gmail.com>
2025-03-25 12:18:35 -0400

    "Renombrado de columna 'last_fm_user_id' a 'user_id' en la migración y ajustes en las claves foráneas para reflejar el cambio."

--- database/migrations/2025_03_25_144047_create_last_fm_track_last_fm_weekly_chart.php
+++ database/migrations/2025_03_25_144047_create_last_fm_track_last_fm_weekly_chart.php
@@ -2,9 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Models\LastFm\Track;
-use App\Models\LastFm\WeeklyChart;
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,7 +13,7 @@ return new class extends Migration
         Schema::create('last_fm_track_last_fm_weekly_chart', function (Blueprint $table) {
             $table->unsignedBigInteger('last_fm_track_id');
             $table->unsignedBigInteger('last_fm_weekly_chart_id');
-            $table->unsignedBigInteger('last_fm_user_id');
+            $table->unsignedBigInteger('user_id');
             $table->integer('playcount');
 
             $table->foreign('last_fm_track_id', 'fk_track')
@@ -27,7 +24,7 @@ return new class extends Migration
                 ->references('id')
                 ->on('last_fm_weekly_charts');
 
-            $table->foreign('last_fm_user_id', 'fk_user')
+            $table->foreign('user_id', 'fk_user')
                 ->references('id')
                 ->on('users');
 
